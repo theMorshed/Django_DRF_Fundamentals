56931ad4871371d22642ffa1a7b5bc1e50561168
theMorshed <themorshedctg@gmail.com>
2023-10-13 12:05:00 +0600

    single article add, update, delete functional api created

--- article_api/urls.py
+++ article_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from article_api.views import article_list
+from article_api.views import article_list, article_detail
 
 urlpatterns = [
     path('article/', article_list),
+    path('details/<int:pk>/', article_detail)
 ]
--- article_api/views.py
+++ article_api/views.py
@@ -1,12 +1,44 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from article_api.models import Article
 from article_api.serializers import ArticleSerializer
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
+@csrf_exempt
 def article_list(request):
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many = True)
         return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
         
+        
+@csrf_exempt
+def article_detail(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(article, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        article.delete()
+        return HttpResponse(status=204)
